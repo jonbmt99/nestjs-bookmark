31a70cee8b0c8f674d3637e695d668397f91b16f
Truong Nguyen <mrherik@gmail.com>
2022-04-16 03:06:56 +0700

    add-docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,28 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import { AuthModule } from './auth/auth.module';
 import {AddressModule} from "./address/address.module";
 import {UserModule} from "./user/user.module";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'P@ssword123',
+    //   database: 'bookmark',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    //   }
+    // ),
+    ConfigModule.forRoot({isGlobal: true}),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'P@ssword123',
-      database: 'bookmark',
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
-      }
-    ),
+    }),
     AuthModule,
     AddressModule,
     UserModule,
